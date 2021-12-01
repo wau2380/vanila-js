20773b0e0b4b7375b51c2f79483b1ec09ec5dd2e
wau2380 <wau2380@gmail.com>
2021-12-02 00:33:57 +0900

    update

---
+++ Graduated/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEYS = "5419510566ae72eda8c5b42d2211b2bc";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEYS}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector(".weather span:first-child");
+      const city = document.querySelector(".weather span:nth-child(2)");
+      const temp = document.querySelector(".weather span:last-child");
+      temp.innerText = data.main.temp;
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
