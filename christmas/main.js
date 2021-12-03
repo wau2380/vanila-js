9b0237f2a1ce975fa96e7083edc23de8d51a40e6
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2021-12-03 10:58:06 +0900

    update

--- christmas/main.js
+++ christmas/main.js
@@ -15,3 +15,21 @@ setInterval(function () {
   document.getElementById("count").innerHTML =
     "Time Until Chrismas " + day + "d " + hour + "h " + min + "m " + sec + "s";
 }, 1000);
+
+setInterval(createSnow, 100);
+ 
+function createSnow() {
+   const snow = document.createElement('i');
+   snow.classList.add('fas');
+   snow.classList.add('fa-snowflake');
+   snow.style.left = Math.random() * window.innerWidth + 'px';
+   snow.style.animationDirection = Math.random() * 3 + 2 + 's';
+   snow.style.opacity = Math.random();
+   snow.style.fontSize = Math.random() * 10 + 10 + 'px';
+ 
+   document.body.appendChild(snow);
+ 
+   setTimeout(() => {
+      snow.remove();
+   }, 5000);
+}
