a05b0aacfb7676fe98a646754018c9971e3a109a
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2021-12-08 10:26:20 +0900

    update

--- Graduated/js/clock.js
+++ Graduated/js/clock.js
@@ -5,7 +5,7 @@ function Time() {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+  clock.innerHTML = `${hours }:${minutes}:${seconds}`;
 }
 
 setInterval(Time, 1000);
